c626a1ceaeced5411688acc2c4c9d17b0601a7af
MRK5605 <MRK5605@R5091839>
2017-02-08 18:17:32 -0600

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yoour name is?...");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How much sleep?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name);
+            if (hoursOfSleep < 8)
+            {
+                Console.WriteLine("You need to get more sleep");
+            } 
+            else
+            {
+                Console.WriteLine("You are well rested.");
+            }
+            
+        }
+    }
+}
